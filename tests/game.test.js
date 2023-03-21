a37d192bd14aae9896cbca791a9a304875443595
A-Gr33n <aarongreen193@hotmail.com>
2023-03-21 00:12:01 +0000

    Added newGame Function

--- tests/game.test.js
+++ tests/game.test.js
@@ -2,15 +2,16 @@
  * @jest-environment jsdom
  */
 
+const { game, newGame, showScore} = require("node:test");
 const { game } = require("../game");
 
 
 beforeAll(() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
+   game.score=42 ;
+   game.playerMoves = ["button1" , "button2"] ;
+   game.currentGame =  ["button1" , "button2"] ;
+   document.getElementById("score").innerText = "42" ; 
+   newGame();  
 });
 
 describe("game object contains correct keys", () => {
@@ -29,4 +30,12 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+    test("should display 0 for the element with id of score" , () =>{
+        expect (document.getElementById ("score").innerText).toEqual(0)
+    })
 });
+
+describe("newGame works correctly," () =>
+{
+      expect(game.score).toEqual(0);
+})
